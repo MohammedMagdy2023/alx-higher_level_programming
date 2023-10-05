b13f6d6068c482f56907c69872387aec69a24b5d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-05 19:48:44 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv) #This gives the n arguments including the script name
-    argv = sys.argv[1:]
+    argc = len(sys.argv)  # This gives the n arguments including the script name
+    argv = sys.argv[1:]   # This creats a list of n arguments without the script name
     if argc > 1:
-        if argc == 1 :
-                j = 1
-                print("{} argument".format(len(argv)))
-                print("{} : {}".format(j, argv))
-        else:
+        if argc == 1:  # This prints the argument if there is one
+            j = 1
+            print("{} argument".format(len(argv)))
+            print("{} : {}".format(j, argv))
+        else:  # This prints the n number of arguments
             j = 1
             print("{} arguments".format(len(argv)))
             for i in argv:
                 print("{} : {}".format(j, i))
                 j += 1
-    else:
+    else: # This prints the argument if there is no arguments
         print(" {} arguments.".format(0))
