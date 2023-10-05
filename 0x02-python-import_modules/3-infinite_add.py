ff0833be3ec2c7438b98c6a3a01c74dbc8aaba76
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-05 21:28:14 +0300

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    arg_c = len(sys.argv[1:]
+    arg_c = len(sys.argv[1:])
     res = 0
     for i in sys.argv[1:]:
                 res += int(i)
     print(res)
-    
