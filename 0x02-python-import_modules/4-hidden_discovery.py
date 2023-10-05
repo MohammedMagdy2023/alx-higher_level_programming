1ac2bfdb2c66b6b2daa8f862d9eaa8173178f6a2
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-05 21:47:05 +0300

    Hidden_Discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    NameList = dir(hidden_4)
+    for i in NameList:
+        if "__" not in i:
+            print("{}".format(i))
