8692a99ce94054c136ace98990a457aee5aad067
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-05 23:30:45 +0300

    My_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_c = len(sys.argv[1:])
+    arg_v = sys.argv[1:]
+    if arg_c != 3:
+        exit(1)
+    else:
+        a = sys.argv[2]
+        b = sys.ergv[4]
+        operator = sys.argv[3]
+        match operator:
+            case '+':
+                print("{} + {} = {}".format(a, b, int(a) + int(b)))
+            case '-':
+                print("{} - {} = {}".format(a, b, int(a) - int(b)))
+            case '*':
+                print("{} * {} = {}".format(a, b, int(a) * int(b)))
+            case '/':
+                print("{} / {} = {}".format(a, b, int(a) / int(b)))
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
