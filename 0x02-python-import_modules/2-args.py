10a59bc6f2af9147eb7744840839f01f816ab05f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-05 19:43:04 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) #This gives the n arguments including the script name
+    argv = sys.argv[1:]
+    if argc > 1:
+        if argc == 1 :
+                j = 1
+                print("{} argument".format(len(argv)))
+                print("{} : {}".format(j, i))
+        else:
+            j = 1
+            print("{} arguments".format(len(argv)))
+            for i in argv:
+                print("{} : {}".format(j, i))
+                j += 1
+    else:
+        print(" {} arguments.".format(0))
