cdee24ea5f6cf4961a2a941806ae3acee58581ae
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-08 19:36:49 +0300

    Remove_letter_c_from_string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_list = ''
+    for i in range(0,len(my_string)):
+        if my_string[i] == "c":
+            continue
+        else:
+            new_list += my_string[i]
+    return new_list
