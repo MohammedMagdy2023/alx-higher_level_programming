f4dae3a2412a93532a298be049ecef12624c3803
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-09 23:45:45 +0300

    Print_matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) < 0:
+        return None
+    else:
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i])):
+                print(matrix[i][j], end= "")
+            print()
