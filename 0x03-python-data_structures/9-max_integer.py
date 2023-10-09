e5a67eaf413912972a4efe418372e19034ac2316
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-09 17:48:19 +0300

    Max_in_list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    Comp_num = my_list[0]
     if not my_list:
         return None
     else:
+        Comp_num = my_list[0]
         for i in my_list:
             if i > Comp_num:
                 Comp_num = i
