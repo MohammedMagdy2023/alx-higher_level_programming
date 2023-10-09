5f239df3f6672a9c6d55b4acfcddc8f20c787da5
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-09 17:40:15 +0300

    Max_in_list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    Comp_num= 0
+    if False:
+        return None
+    else:
+        for i in my_list:
+            if i > Comp_num :
+                Comp_num = i
+            else:
+                continue
+        return Comp_num
