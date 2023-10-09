83fbef5f5df369d5d77333d0821b4b4edacc50a6
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-09 23:55:58 +0300

    Print_matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for i in matrix:
             for j in i:
-                print(" ".join("{:d}".format(j)))
+                print(" ".join("{:d}".format(j)), end="")
             print()
