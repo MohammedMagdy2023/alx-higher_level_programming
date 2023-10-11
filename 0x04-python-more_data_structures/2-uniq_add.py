cca5e85fd1d53623ec29403019d49ff03faf26f3
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 01:14:56 +0300

    UNIQ_ONLY

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if not my_list:
+        return my_list
+    else:
+        res = 0
+        checked = []
+        for i in my_list:
+            if i in checked:
+                continue
+            else:
+                res += i
+                checked.append(i)
+    return res
