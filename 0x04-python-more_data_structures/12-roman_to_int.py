ce32e928584d48d15cb04bd8fff68c0c746a2335
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 04:48:18 +0300

    Roman_To_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    character = ["I", "V", "X", "L", "C", "D", "M"]
-    INTIGER = [1, 5, 10, 50, 100, 500, 1000]
-    dict_guid = dict(zip(character, INTIGER))
-    res = 0
-    for i in roman_string:
-        res += dict_guid.get(i)
-    return res
+    if type(roman_string) != str or type(roman_string) == None:
+        return 0
+    else:
+        character = ["I", "V", "X", "L", "C", "D", "M"]
+        INTIGER = [1, 5, 10, 50, 100, 500, 1000]
+        dict_guid = dict(zip(character, INTIGER))
+        res = 0
+        for i in roman_string:
+            res += dict_guid.get(i)
+        return res
