f69c5e4edcf9666569b2a02a5c39655ca0c19021
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 04:54:24 +0300

    Roman_To_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,9 @@ def roman_to_int(roman_string):
         INTIGER = [1, 5, 10, 50, 100, 500, 1000]
         dict_guid = dict(zip(character, INTIGER))
         res = 0
-        for i in roman_string:
-            res += dict_guid.get(i)
+        for i in range(len(roman_string)):
+            if i > 0 and dict_guid[roman_string[i]] > dict_guid[roman_string[i - 1]]:
+                res += dict_guid[roman_string[i]] - 2 * dict_guid[roman_string[i - 1]]
+            else:
+                res += dict_guid[roman_string[i]]
         return res
