8a5eb13154e90060e45c8d6edb11ef5939e669a8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 04:58:44 +0300

    Roman_To_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,10 @@ def roman_to_int(roman_string):
         dict_guid = dict(zip(character, INTIGER))
         res = 0
         for i in range(len(roman_string)):
-            if i > 0 and dict_guid[roman_string[i]] > dict_guid[roman_string[i - 1]]:
-                res += dict_guid[roman_string[i]] - 2 * dict_guid[roman_string[i - 1]]
+            if (i > 0 and 
+                dict_guid[roman_string[i]] > dict_guid[roman_string[i - 1]]):
+                res += (dict_guid[roman_string[i]] - 
+                        2 * dict_guid[roman_string[i - 1]])
             else:
                 res += dict_guid[roman_string[i]]
         return res
