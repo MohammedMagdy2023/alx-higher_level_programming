252861f8ce32a213832a326aa87d944938be8149
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 04:04:35 +0300

    Best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,4 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        c_value = 0
-        for key in a_dictionary.keys():
-            if a_dictionary[key] >= c_value:
-                c_value = a_dictionary[key]
-            return key
-            break
+        return max(a_dictionary)
