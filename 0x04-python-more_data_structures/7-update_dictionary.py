64b1b272b3cddbf9bbb68f578df6763cee629f36
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 03:11:46 +0300

    update_dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary.get(key) != None:
+        a_dictionary.update(key, value)
+    else:
+        a_dictionary[key] = value
