485358bee21022786c7e1b712b723f58d4973573
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 03:59:34 +0300

    Best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary[key] == None:
+        return None
+    else:
+        c_value = 0
+        for key in a_dictionary.keys():
+            if a_dictionary[key] >= c_value:
+                c_value = a_dictionary[key]
+            return key
+            break
