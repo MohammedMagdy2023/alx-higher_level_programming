f0554ba283c4782a3afef9a0847ea8319372d04a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 05:33:22 +0300

    avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        n1 = n2 = 0
+        for i, j in my_list:
+            n1 += i * j
+            n2 += j
+        avg = n1 / n2
+        return avg
