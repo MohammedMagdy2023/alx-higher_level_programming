ef0ec7a2d89330b4fa040faa921e2fd8f3651b9c
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-12 03:27:47 +0300

    Delete_key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    KEY_ERROR = a_dictionary.get(key, 1)
+    if KEY_ERROR != 1:
+        del a_dictionary[key]
+    return a_dictionary
