e35c188f562173a44340c2f011a820cdea87b935
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-24 01:55:54 +0300

    safe_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             i+=1
         except IndexError:
-            break
+            pass
     print()
     return i
