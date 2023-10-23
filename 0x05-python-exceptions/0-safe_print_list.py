19dceebdb79e7bcea5f0809b5fda8371699ffb11
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-24 01:49:10 +0300

    safe_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x+1):
+    i = 0
+    while i <= x:
         try:
             print("{:d}".format(my_list[i]), end="")
         except ValueError:
             pass
+        i += 1 
     print()
     return i
