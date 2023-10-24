397a978dda6851e0f06cff08300fdb7475e23168
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-24 23:56:49 +0300

    Size_validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
         Instantiation with optional size: def __init__(self, size=0):
         size must be an integer, otherwise raise a TypeError exception
         with the message size must be an integer
-        if size is less than 0, 
+        if size is less than 0,
         raise a ValueError exception with the message size must be >= 0
     """
     def __init__(self, size=0):
