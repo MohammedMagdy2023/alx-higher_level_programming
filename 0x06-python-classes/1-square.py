e5bfa124dfe99ef876694376d83c4ff8a41b3062
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-24 23:19:09 +0300

    private_size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class Startes here"""
+
+
+class Square:
+    """Create an class named Square and create 
+        and private attribute named size
+    """
+    def __init__(self, size):
+        self.__size = size
