0c07deebc6f7b4159a2f47c9ff137fca7ca030b8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-25 02:47:27 +0300

    position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,6 +18,7 @@ class Square:
     with the character #:
     if size is equal to 0, print an empty line
     """
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -32,19 +33,18 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
-    def size(self):
+    def position(self):
         return self.__position
 
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(i, int) for i in value) or
-                not all(i >= 0 for i in value)):
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -52,12 +52,12 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
+
         [print("") for i in range(0, self.__position[1])]
-        for _ in range(0, self.__size):
+        for i in range(0, self.__size):
             [print(" ", end="") for _ in range(0, self.__position[0])]
             [print("#", end="") for _ in range(0, self.__size)]
             print("")
