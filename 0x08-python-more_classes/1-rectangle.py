ff72f8b2f4ed8a5e2b9b41d6bebd31ed1134e52b
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-31 00:21:12 +0200

    real_definition

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+class Rectangle:
+    """P"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__hieght=height
+
+    @property #getter
+    def width(self):
+        return self.__width
+
+    @property #getter
+    def height(self):
+        return self.__hieght
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__hieght = value
