31f9c8850eb3d732a26795b8b1811fa350f4db44
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-31 04:54:27 +0200

    Change_representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -11,7 +11,6 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
@@ -80,7 +79,6 @@ class Rectangle:
             if i != self.height - 1:
                 res.append("\n")
         return ("".join(res))
-
 
     def __repr__(self):
         """
