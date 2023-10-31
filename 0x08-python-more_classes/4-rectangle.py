99af4555d039df60cb1d83370cdcbb1cb3992bf0
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-31 03:35:55 +0200

    Eval_is_magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -65,4 +65,7 @@ class Rectangle:
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
+        """
+        return the usage for the rectangle class 
+        """
         return "Rectangle({}, {})".format(self.__width, self.__height)
