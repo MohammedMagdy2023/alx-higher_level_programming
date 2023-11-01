c361fffa3461caa9621e5f82622039a3ceff7960
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-01 05:20:01 +0200

    doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,17 +10,8 @@ def add_integer(a, b=98):
     """
     add_integer return the sum of two integer numbers
     """
-
-    c_a = isinstance(a, int) or isinstance(a, float)
-    c_b = isinstance(b, int) or isinstance(b, float)
-    if c_a and c_b:
-        return int(a) + int(b)
-    else:
-        if not c_a and not c_b:
-            raise TypeError("a and b must be integers or floats")
-        elif not c_a:
-            raise TypeError("a must be an integer")
-        elif not c_b:
-            raise TypeError("b must be an integer")
-        else:
-            Exception("Unkown Error happened")
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
