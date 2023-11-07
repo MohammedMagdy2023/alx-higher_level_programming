4a4d66956320d44e8a98c4e1a0c878d63df4594a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-07 04:09:03 +0200

    4-inherits_from.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """implement area function"""
+    def area(self):
+        raise Exception("area() is not implemented")
