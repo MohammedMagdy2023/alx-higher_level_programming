55e33888f27b17323af28c38cc200e9b366dce83
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-07 04:57:17 +0200

    sequare

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Rectangle subclass."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """create a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
