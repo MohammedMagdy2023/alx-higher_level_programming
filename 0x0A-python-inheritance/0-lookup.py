203e498e2abf9620f2e9ca10a7833d54e5951f13
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-07 02:52:10 +0200

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+a function that returns the list of available attributes and methods:
+Prototype: def lookup(obj):
+Returns a list object
+"""
+
+
+def lookup(obj):
+    """Return the all available methods and attributes"""
+    return dir(obj)
