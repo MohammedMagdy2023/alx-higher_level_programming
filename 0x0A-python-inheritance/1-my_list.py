b5d8ea37a1ee4899e47dceb8d5adacfa9fb9efaa
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-06 17:06:20 -0800

    mylist

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int
+You are not allowed to import any module
+"""
+
+
+class Myllist(list):
+    """ a class MyList that inherits from list"""
+    super().__init__()
+    def print_sorted(self):
+        pass
