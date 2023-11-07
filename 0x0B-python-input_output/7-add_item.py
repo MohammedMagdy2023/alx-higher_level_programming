93a53797282dfe4a93cbedfa1fd6ed32dc7b2e36
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 01:52:14 +0200

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Load, add, save"""
 import sys
-save_to_json_file=__import__("5-save_to_json_file.py").save_to_json_file
-load_from_json_file=__import__("6-load_from_json_file.py").load_from_json_file
+save_to_json_file=__import__("5-save_to_json_file").save_to_json_file
+load_from_json_file=__import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
     filename = "add_item.json"
