5b993861deda02cfc8a0fde7d5c4d9ca373466c9
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 01:52:57 +0200

    7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Load, add, save"""
 import sys
-save_to_json_file=__import__("5-save_to_json_file").save_to_json_file
-load_from_json_file=__import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
     filename = "add_item.json"
-    my_list = load_from_json_file(filename)
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
     my_list.extend(sys.argv[1:])
     save_to_json_file(my_list, filename)
