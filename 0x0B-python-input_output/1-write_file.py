b3b08257356db85bda595459eb6bbeb660b06ce7
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 00:31:03 +0200

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Writing into a file"""
+
+
 def write_file(filename="", text=""):
     """A function that write test to a file and count it"""
     with open(filename, "w", encoding="UTF-8") as f:
