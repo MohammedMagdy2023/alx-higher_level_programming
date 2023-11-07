2e17403b3621347946996ca56d259f065d0e117d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-07 22:16:49 +0200

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """ a function that reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read())
