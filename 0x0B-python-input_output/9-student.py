e6aff36cce948c8b2cb0d4eb436bf0cbc6a99cee
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 02:16:48 +0200

    9-student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """class to json"""
-import json
 
 
 class Student:
@@ -10,9 +9,10 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Public instance attributes:
-        first_name
-        last_name
-        age
+        Arguments:
+            first_name: The first name of the student.
+            last_name : The last name of the student.
+            age: The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
