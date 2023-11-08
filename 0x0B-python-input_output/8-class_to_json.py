5f58a5081a819a4524652b5e8632f15601abbc31
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 02:00:51 +0200

    8-class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class to JSON"""
+
+def class_to_json(obj):
+    """
+    returns the dictionary description for JSON serialization of an object
+    """
+    return obj.__dict__
