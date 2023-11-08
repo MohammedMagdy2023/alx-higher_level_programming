ad4043a1760bbc1495b8d0a60bbaf14c11fbea80
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 02:36:09 +0200

    12-pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    returns list of lists of integers representing the Pascal’s triangle of n:
+    """
+
+    if n <= 0:
+        return []
+    else:
+        pass
