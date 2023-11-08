8afc32f8e48a9a0d5ae3fa5e960689c9d79b28c0
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-08 02:15:05 +0200

    9-student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,21 +5,22 @@ import json
 
 class Student:
     """
-    a class Student that defines a student by:
-    Public instance attributes:
-    first_name
-    last_name
-    age
-    Instantiation with first_name, last_name and age:
-    def __init__(self, first_name, last_name, age):
-    Public method def to_json(self):
-    that retrieves a dictionary representation of a Student instance
+    class Student that defines a student by:
     """
-
     def __init__(self, first_name, last_name, age):
+        """
+        Public instance attributes:
+        first_name
+        last_name
+        age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        Public method def to_json(self):
+        that retrieves a dictionary representation of a Student instance
+        """
         return self.__dict__
