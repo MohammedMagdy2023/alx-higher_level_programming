fa596235df9359a37088358f4eafb9d8e25ff16d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-13 00:31:50 +0200

    indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Text indentation"""
+
+
+def text_indentation(text):
+    """
+    a function that prints a text with 2 new lines after
+    each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        replacements = ['.', '?', ':']
+        for char in replacements:
+            text = text.replace(char, char + '\n\n')
+        lines = text.split('\n')
+        lines = [line.strip() for line in lines]
+        text = '\n'.join(lines)
+        print(text)
