1f70515cfc1363ef4c0af80fbd335ce9439af1bb
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-14 01:51:40 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,7 +30,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """The size Private instance getter"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -39,7 +39,8 @@ class Square(Rectangle):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """return the string representation of the class"""
