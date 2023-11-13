a1d9cbde989b3a7df661eb9c2499fe89060bfb58
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-14 01:53:54 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,11 +34,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """The size Private instance setter"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
         self.width = value
         self.height = value
 
