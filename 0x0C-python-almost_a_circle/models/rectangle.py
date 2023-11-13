150911f94203e9d23df6bb15578a3ea030c7f7f7
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-13 22:46:42 +0200

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,14 +6,24 @@ from models.base import Base
 class Rectangle(Base):
     """
     The Rectangle Class thaat inherit from base
-    Private instance attributes, each with its own public getter and setter:
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        The init of the new class instances
+        Args:
+            width: The width of the new Rectangle.
+            height: The height of the new Rectangle.
+            x: The x of the new Rectangle.
+            y: The y of the new Rectangle.
+            id: The id of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+
         super().__init__(id)
         self.width = width
         self.height = height
