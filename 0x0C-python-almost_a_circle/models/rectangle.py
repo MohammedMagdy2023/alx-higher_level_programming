d3819cb9b7b5ee190df1d15e486b2e6edeaa501d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-13 23:05:34 +0200

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,4 +109,4 @@ class Rectangle(Base):
     def __str__(self):
         """return the string representation of the class"""
         return "[Rectangle] ({}) {}/{} - {}/{}"\
-            .format(self.id,self.x, self.y,self.width, self.height)
+            .format(self.id, self.x, self.y, self.width, self.height)
