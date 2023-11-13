7eea8b8667edd759b7d45a3d0dd443905cfb8175
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-13 23:38:13 +0200

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,11 +40,9 @@ class Rectangle(Base):
         """width property setter"""
         if not isinstance(value, int):
             raise TypeError("width must be integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -56,10 +54,9 @@ class Rectangle(Base):
         """height property setter"""
         if not isinstance(value, int):
             raise TypeError("height must be integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -71,10 +68,9 @@ class Rectangle(Base):
         """x property setter"""
         if not isinstance(value, int):
             raise TypeError("x must be integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -86,10 +82,9 @@ class Rectangle(Base):
         """y property setter"""
         if not isinstance(value, int):
             raise TypeError("y must be integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
         """calculate the area of the rectangle"""
@@ -115,7 +110,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update the Rectangle.
 
         Args:
