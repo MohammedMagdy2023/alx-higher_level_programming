f1d8a5ecec7a3f5d1d09a37e17a9f9de3c4beec0
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-14 02:10:45 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -42,16 +42,14 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
 
-def update(self, *args, **kwargs):
+    def update(self, *args, **kwargs):
         """Update the Rectangle.
 
         Args:
-            *args (ints): New attribute values.
-                - 1st argument is the id attribute
-                - 2nd argument is the width attribute
-                - 3rd argument is the height attribute
-                - 4th argument is the x attribute
-                - 5th argument is the y attribute
+            1st argument should be the id attribute
+            2nd argument should be the size attribute
+            3rd argument should be the x attribute
+            4th argument should be the y attribute
         """
         if args and len(args) != 0:
             i = 0
