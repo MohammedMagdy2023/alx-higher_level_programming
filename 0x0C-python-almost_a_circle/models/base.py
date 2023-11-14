9d8f78c6a3dbde4db35667d1c91976fecbf4ac65
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-14 03:13:24 +0200

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """The base class is here"""
+import json
 
 
 class Base:
@@ -22,3 +23,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
