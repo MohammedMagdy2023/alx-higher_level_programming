c21724cceea725b85a55ac83e47446b2be4b261c
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-08 18:58:11 -0800

    second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-if (args[0] === undefined || Number(args[0]) === 1) {
+if (args[0] === undefined || args[0] === 1) {
   console.log(0);
 } else {
   Number(args);
