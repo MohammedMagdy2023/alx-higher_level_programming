a09fa42bd866a239ec5122f68369aa8566aac017
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-08 17:55:00 -0800

    to Number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-if (Number(args[0]) == undefined) {
+if (args[0] === undefined || isNaN(args[0])) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + Number(args[0]));
