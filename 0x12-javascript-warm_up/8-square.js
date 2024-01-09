07234a0b7575e7350e816606971b3cb8521294d1
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-08 18:37:44 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ const size = args[0];
 if (size === undefined || isNaN(size)) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < Number(size); i++) {
-      console.log('X'.repeat(size));
-    }
+  for (let i = 0; i < Number(size); i++) {
+    console.log('X'.repeat(size));
+  }
 }
