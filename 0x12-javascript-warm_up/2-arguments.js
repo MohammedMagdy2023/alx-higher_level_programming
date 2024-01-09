bc6a1edf5842014b7953adf4bff542dbac954b08
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-08 16:49:36 -0800

    argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let args = process.argv.slice(2);  // Remove the first two elements
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
