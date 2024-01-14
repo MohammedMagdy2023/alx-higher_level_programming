5940bdb34f75297fdc13621b68a4bf85bb93f746
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-14 11:02:28 -0800

    select_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" select all values starts with N """
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE = sys.argv[3]
+
+    conn = sql.connect(host = "localhost",
+                        port = 3306,
+                        username = USERNAME,
+                        password = PASSWORD,
+                        database = DATABASE
+                        charset = "uft8")
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
