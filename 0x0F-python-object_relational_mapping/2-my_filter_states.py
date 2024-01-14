f5c517d8a552f03a1c148907a011465eeeddaab1
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-14 15:00:31 -0800

    filter user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" Query database based on user input from argv list """
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    conn = sql.connect(host="localhost",
+                       port=3306,
+                       user=sys.argv[1],
+                       passwd=sys.argv[2],
+                       db=sys.argv[3],
+                       charset="utf8")
+    cur = conn.cursor()
+    cur.execute(f"""SELECT * FROM states
+                WHERE name={sys.argv[4]}
+                ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
