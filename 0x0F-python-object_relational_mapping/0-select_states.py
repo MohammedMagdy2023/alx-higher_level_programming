8f4b2dbedae583adb10ea5893d58a11660aa8c5b
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-14 08:15:43 -0800

    Select all

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Select states of the Mysql database in ascending order"""
+
+
+import MySQLdb as sql
+import sys
+
+USERNAME = sys.argv[1]
+PASSWORD = sys.argv[2]
+DATABASE = sys.argv[3]
+
+conn = sql.connect(
+host="localhost",
+port=3306,
+user=USERNAME,
+passwd=PASSWORD,
+db=DATABASE,
+charset="utf8"
+)
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
