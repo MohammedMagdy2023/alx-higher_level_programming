38dbaa0f9093c07b7ffbbae548131952b07ab2c3
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-14 16:36:04 -0800

    secure filter user input

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" Query database based on user input from argv list """
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    conn = sql.connect(host="localhost",
+                       port=3306,
+                       user=sys.argv[1],
+                       passwd=sys.argv[2],
+                       db=sys.argv[3],
+                       charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY %s
+                ORDER BY states.id""", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
