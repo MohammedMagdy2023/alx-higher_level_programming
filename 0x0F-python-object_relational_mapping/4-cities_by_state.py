9136dbb091f27e706ab9ff2506c0161848465713
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-15 19:01:50 -0800

    ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Query database based on user input from argv list """
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    conn = sql.connect(host="localhost",
+                       port=3306,
+                       user=sys.argv[1],
+                       passwd=sys.argv[2],
+                       db=sys.argv[3],
+                       charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""preparing to Start using the ORM in python """
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """The Database module using ORM"""
+
+
+    __tablename__ = 'State'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(128), nullable=False)
