6a5016031e136c4f46696ffac6eb352f93bf1cb1
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-25 19:05:37 -0800

    POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use Post method to send data
-curl -sX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
