7a7847b2e3d9abf2621c4aceb2489ff293e94a0a
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-27 07:26:57 -0800

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (args.length <= 1) {
   console.log(0);
 } else {
   let arr = args.map(Number);
-  let set = new Set(arr);
+  const set = new Set(arr);
   arr = Array.from(set);
   arr.sort((a, b) => a - b);
   console.log(arr[arr.length - 2]);
