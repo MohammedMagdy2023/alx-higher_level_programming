77e63b51e7993816cab25d05ff2c1a61e8c4949f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-26 17:27:28 -0800

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Create a request to a web server """
-from urllib.request import urlopen
 
 
-with urlopen('https://alx-intranet.hbtn.io/status') as conn:
-    resp = conn.read()
-    page_type = type(resp)
-    utf_content = resp.decode("UTF-8")
-    print(f"""Body response:
-    - type: {page_type}
-    - content: {resp}
-    - utf8 content: {utf_content}""")
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    with urlopen('https://alx-intranet.hbtn.io/status') as conn:
+        resp = conn.read()
+        page_type = type(resp)
+        utf_content = resp.decode("UTF-8")
+        print(f"""Body response:
+        - type: {page_type}
+        - content: {resp}
+        - utf8 content: {utf_content}""")
