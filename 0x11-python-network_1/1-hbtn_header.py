2e4e493fee6c57204a157f905cc4ed1977709d9c
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-27 01:47:57 -0800

    urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Create a request to a web server """
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(url = sys.argv[1]) as conn:
+        print(dict(conn.headers).get("X-Request-Id"))
