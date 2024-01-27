684aba495a6907b1fa04400e88945e7ac27e7b66
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-26 17:23:53 -0800

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@ from urllib.request import urlopen
 
 with urlopen('https://alx-intranet.hbtn.io/status') as conn:
     resp = conn.read()
-    type = type(resp)
+    page_type = type(resp)
     utf_content = resp.decode("UTF-8")
     print(f"""Body response:
-    - type: {type}
+    - type: {page_type}
     - content: {resp}
     - utf8 content: {utf_content}""")
