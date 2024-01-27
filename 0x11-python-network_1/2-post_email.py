d53ff899a55193c7c2cf98913f8a48d7482f1b17
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-01-26 19:20:50 -0800

    urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Create a request to a web server """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    data = {
+        "email": sys.argv[2],
+    }
+
+    # Encode the data
+    data = urllib.parse.urlencode(data)
+    data = data.encode("UTF-8")
+
+    # Create the request
+    url = sys.argv[1]
+    req = urllib.request.Request(url, data=data, method="POST")
+
+    # Send the request and get the response
+    response = urllib.request.urlopen(req)
+
+    # Print the response content
+    print(response.read())
