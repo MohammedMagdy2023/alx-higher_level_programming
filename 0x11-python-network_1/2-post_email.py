4cf77a00d0c4fbc7760b32ea9ced3a6a138490e8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-04 15:54:33 -0800

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,20 +7,11 @@ if __name__ == "__main__":
     import urllib.parse
     import sys
 
-    data = {
-        "email": sys.argv[2],
-    }
-
-    # Encode the data
-    data = urllib.parse.urlencode(data)
-    data = data.encode("UTF-8")
-
-    # Create the request
     url = sys.argv[1]
-    req = urllib.request.Request(url, data=data, method="POST")
+    data = {'email' : sys.argv[2]}
 
-    # Send the request and get the response
-    response = urllib.request.urlopen(req)
-
-    # Print the response content
-    print(response.read().decode("UTF-8"))
+    params = urllib.parse.urlencode(data)
+    params = params.encode('ascii') # data should be bytes
+    req = urllib.request.Request(sys.argv[1], params)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
