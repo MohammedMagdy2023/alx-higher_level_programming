8499c6d6b16a2a95ffeaeb3cecb2a108910810cd
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-20 17:19:34 -0800

    count response4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    response = JSON.parse(response.body);
+    console.log(response.films.length);
+  }
+});
