28fda3f15763e78c96f5323de2c14292be8de77d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-20 17:37:38 -0800

    compeleted tasks6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const res = JSON.parse(body);
+    const count = {};
+
+    for (const task of res) {
+      const userId = task.userId;
+      const completed = task.completed;
+
+      if (completed) {
+        count[userId] = (count[userId] || 0) + 1;
+      }
+    }
+
+    for (const userId in count) {
+      console.log(`${userId}: ${count[userId]}`);
+    }
+  }
+});
